2a10705f9d6f5440559fb106d6fbdcdee2b0ffe4
Patrik <patriktao@gmail.com>
2022-05-15 04:11:27 +0200

    Lab 2 Klar

---
+++ lab2/src/edaa40/random.clj
@@ -0,0 +1,96 @@
+(ns edaa40.lab2)
+(use 'clojure.set)
+
+(def TinyWeb #{[0 1] [0 2] [1 2] [2 1] [2 3]})
+
+(defn dom
+  "computes the domain of a relation"
+
+  [R]
+
+  (set (for [r R] (first r))))
+
+(defn rng
+  "computes the range of a relation"
+
+  [R]
+
+  (set (for [r R] (second r))))
+
+(defn image-of
+  "computes the image of the element x under R"
+
+  [R x]
+
+  (set
+   (for [p R :when (= x (first p))]
+     (second p))))
+
+(dom TinyWeb)
+(rng TinyWeb)
+
+(defn all-pages
+  "computes set of all pages on the web"
+
+   [web]
+   (union (dom web) (rng web))
+;;   hint: check out the Clojure function "union"
+)
+(all-pages TinyWeb) 
+;;(test? "all-pages" (all-pages TinyWeb) #{0 1 2 3})
+
+(defn no-links? 
+  "determines whether a page in a web has no outgoing links"
+  
+  [web page] 
+
+  (contains? (dom web) page)
+)
+
+(no-links? TinyWeb 3)
+
+(defn random-page 
+  "picks a random page occurring in a web"
+
+  [web] 
+
+  (rand-nth (seq(all-pages web)))
+  
+;;   hint: Check out "rand-nth". Also, if you need to convert a set into a sequence, 
+;;         you want to take a look at "seq".
+)
+
+(number? (random-page TinyWeb))
+
+
+(defn random-link 
+ 
+  "picks a random outgoing link from a page in a web,
+  returns the page the link points to"
+  
+  [web page] 
+  
+  ;;få fram de länkarna där outgoing noden är page
+
+  (rand-nth (seq (for [w web :when (= page (first w))] 
+    w
+  )))
+
+;;   hint: If you've done the previous one, this one should be easy.
+)
+
+
+(random-link TinyWeb 0)
+
+(defn surf
+
+  "simulates one step by a surfer, 
+  with boredom probability 1-alpha,
+  returns the page the surfer goes to"
+
+  [web current]
+  
+  (if (< (rand 1) alpha) (random-page web) (if (no-links? web current) (random-page web) (random-link web current)))
+
+  ;;hint: check out "rand"
+)
